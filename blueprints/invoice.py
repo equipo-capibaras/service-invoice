060ac1a7d92fd95444adf658e1780b1df9173853
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-11-06 19:56:21 -0500

    Conexión con SVC_CLIENT funciona

--- blueprints/invoice.py
+++ blueprints/invoice.py
@@ -13,12 +13,3 @@ blp = Blueprint('Invoice', __name__)
 class GetInvoice(MethodView):
     init_every_request = False
 
-    def get(self, client_repo: ClientRepository = Provide[Container.client_repo]) -> Response:
-        client_id = 'acfa53b4-58f3-46e8-809b-19ef52b437ed'
-        client = client_repo.get(client_id)
-        answee = {
-            'id': client.id,
-            'name': client.name,
-            'plan': client.plan.value
-        }
-        return json_response(answee, 200)
